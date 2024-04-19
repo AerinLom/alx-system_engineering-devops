f2b5cfba9147aefeb7e1f037e20f9923cec6b31f
AerinLom <aerinlom1@gmail.com>
2024-04-19 09:03:11 -0700

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask from pip3 using puppet
+# Install Flask version 2.1.0 using pip3
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip'
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
