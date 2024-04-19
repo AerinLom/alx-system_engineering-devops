3f05f1c56ee13f4e1dfa6d85d791a9c006a659e2
AerinLom <aerinlom1@gmail.com>
2024-04-19 08:49:30 -0700

    Task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
 # Kills a process named killmenow using puppet
 exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+  path => '/usr/bin:/usr/sbin:/bin'
 }
--- 0x0A-configuration_management/lint.pp
+++
@@ -1,6 +0,0 @@
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
-  options  => ['--no-document'], # Use '--no-document' instead of '--no-rdoc' and '--no-ri'
-}
-
