c3efdbdeacef392c913be510fff91ea6062df8ce
AerinLom <aerinlom1@gmail.com>
2024-06-04 03:09:19 -0700

    Task 0: Subreddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-  """Return the total number of subscribers on subreddit."""
-  api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-  headers = {"User-Agent": "Python Reddit Subscriber checker"}
-  response = requests.get(api_url, allow_redirects=False, headers=headers)
-  if response.status_code == 200:
-    data = response.json()
-    subreddit_subscribers = data['data']['subscribers']
-    return subreddit_subscribers
-  else:
-    return 0
+    """Return the total number of subscribers on subreddit."""
+
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Python Reddit Subscriber checker"}
+    response = requests.get(api_url, allow_redirects=False, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subreddit_subscribers = data['data']['subscribers']
+        return subreddit_subscribers
+    else:
+        return 0
