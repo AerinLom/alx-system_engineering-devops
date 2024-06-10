d87f28c127cdd1e5d0f76729dbc20efee3c2622a
AerinLom <aerinlom1@gmail.com>
2024-06-10 10:44:10 +0000

    Update allowed requests

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,7 +4,6 @@ exec { 'increase_ulimit_nginx':
   command => '/bin/sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   path    => ['/bin', '/usr/bin'],
   onlyif  => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
-  notify  => Exec['nginx_restart'],
 }
 
 exec { 'nginx_restart':
